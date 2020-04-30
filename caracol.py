37b789d111071ab10b4784ac3f99958eee9cd6bf
Ariel <arefome@gmail.com>
2020-04-30 18:23:52 -0500

    Add existing file

--- caracol.py
+++ caracol.py
@@ -1,39 +1,33 @@
 
 
-
-def llenar_matriz(matriz,tam):
+def llenar_matriz(matriz, tam):
+    for i in range(tam):
+        matriz[i] = [None]*tam
     cont = 1
     while(cont <= tam**2):
-        for i in range(tam):
-            matriz[i] = [None]*tam
-    
         for i in range(tam):
             for j in range(tam):
                 matriz[i][j] = str(cont)
                 cont += 1
 
 
-def caracol(matriz,tam):
+def caracol(matriz, tam):
     caracol = ' '
-    for i in range(tam+1):
-        for idx,fila in enumerate(matriz):
+    while(len(matriz) > 0):
+        for idx, fila in enumerate(matriz):
             if idx == 0:
                 caracol += ' '.join(fila)+' '
-                del(matriz[0])
-       
-        if(i == tam):
-            return caracol
-        
-        copia = []  
-        k = 1
-        while(k < (len(matriz[0])+1)):
-            pos = len(fila)- k
+                del(matriz[idx])
+
+        copia = []
+        cont = 1
+        while(cont < (len(fila)+1)):
+            pos = len(fila) - cont
             columna = []
             for fila in matriz:
                 columna.append(fila[pos])
             copia.append(columna)
-            k+=1
-                
+            cont += 1
         matriz = copia
     return caracol
 
@@ -41,5 +35,5 @@ def caracol(matriz,tam):
 if __name__ == '__main__':
     tam = int(input('Tamanio matriz: '))
     matriz = [None]*tam
-    llenar_matriz(matriz,tam)
-    print(caracol(matriz,tam))
+    llenar_matriz(matriz, tam)
+    print(caracol(matriz, tam))
